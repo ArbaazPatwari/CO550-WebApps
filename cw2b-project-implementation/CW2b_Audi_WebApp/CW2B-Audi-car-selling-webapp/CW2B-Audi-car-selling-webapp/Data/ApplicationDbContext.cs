c8c9e98e50de30de720c1ad2c6dd3d60ac360e42
Arbaaz Patwari <arbaazpatwari@gmail.com>
2023-01-12 15:52:32 +0000

    making blank razor pages project
    As a team we have now decided to switch to razor pages for the easier implementation of CRUD pages

---
+++ cw2b-project-implementation/CW2b_Audi_WebApp/CW2B-Audi-car-selling-webapp/CW2B-Audi-car-selling-webapp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CW2B_Audi_car_selling_webapp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
