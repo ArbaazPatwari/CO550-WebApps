8347daacee7e89478249d7e1d46ed1b821977c97
Arbaaz Patwari <arbaazpatwari@gmail.com>
2023-01-19 13:01:27 +0000

    Adding New web app base

---
+++ cw2b-project-implementation/CW2B_RP_Audi_Team_4/CW2B_RP_Audi_Team_4/Data/AudiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CW2B_RP_Audi_Team_4.Models;
+
+namespace CW2B_RP_Audi_Team_4.Data
+{
+    public class AudiContext : DbContext
+    {
+        public AudiContext (DbContextOptions<AudiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CW2B_RP_Audi_Team_4.Models.Car> Car { get; set; } = default!;
+    }
+}
