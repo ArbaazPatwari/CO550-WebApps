e189a3a5f7c0db3d2b8e73d6cd8a6b618e2ee146
BrianRuszkowski <72171622+BrianRuszkowski@users.noreply.github.com>
2023-01-24 16:50:37 +0000

    Added Customer

--- CW2B_RP_Audi_Team_4/Models/Customer.cs
+++ CW2B_RP_Audi_Team_4/Models/Customer.cs
@@ -1,4 +1,8 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace CW2B_RP_Audi_Team_4.Models
@@ -8,5 +12,21 @@ namespace CW2B_RP_Audi_Team_4.Models
         public int CustomerID { get; set; }
 
         public string CustomerName { get; set; } = string.Empty;
+
+        public string CustomerEmail { get; set; } = string.Empty;
+
+        public string CustomerPhone { get; set; } = string.Empty;
+
+        public string CustomerCity { get; set; } = string.Empty;
+
+        public string CustomerRegion { get; set; } = string.Empty;
+
+        public string CustomerAddress { get; set; } = string.Empty;
+
+        public string OrderID { get; set; } = string.Empty;
+
+        public string OrderStatus { get; set; } = string.Empty;
+
+
     }
 }
