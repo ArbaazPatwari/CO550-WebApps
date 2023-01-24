411a8a5f4b60623fd54a6aebf2d3670dc0dd1439
BrianRuszkowski <72171622+BrianRuszkowski@users.noreply.github.com>
2023-01-24 17:08:39 +0000

    Added Scaffold to Customer

--- CW2B_RP_Audi_Team_4/Models/Customer.cs
+++ CW2B_RP_Audi_Team_4/Models/Customer.cs
@@ -9,24 +9,40 @@ namespace CW2B_RP_Audi_Team_4.Models
 {
     public class Customer
     {
+        [Display(Name = "CustomerID")]
+        [StringLength(10)]
         public int CustomerID { get; set; }
 
+        [Display(Name = "CustomerName")]
+        [StringLength(20)]
         public string CustomerName { get; set; } = string.Empty;
 
+        [Display(Name = "CustomerEmail")]
+        [StringLength(40)]
         public string CustomerEmail { get; set; } = string.Empty;
 
+        [Display(Name = "CustomerPhone")]
+        [StringLength(15)]
         public string CustomerPhone { get; set; } = string.Empty;
 
+        [Display(Name = "CustomerCity")]
+        [StringLength(20)]
         public string CustomerCity { get; set; } = string.Empty;
 
+        [Display(Name = "CustomerRegion")]
+        [StringLength(20)]
         public string CustomerRegion { get; set; } = string.Empty;
 
+        [Display(Name = "CustomerAddress")]
+        [StringLength(50)]
         public string CustomerAddress { get; set; } = string.Empty;
 
+        [Display(Name = "OrderID")]
+        [StringLength(10)]
         public string OrderID { get; set; } = string.Empty;
 
-        public string OrderStatus { get; set; } = string.Empty;
+        // Navigation properties
 
-
+        //public virtual ICollection<Customer> Customers { get; set; }
     }
 }
